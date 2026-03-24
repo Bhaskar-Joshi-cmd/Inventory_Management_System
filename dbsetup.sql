c73ace08aecb5f1804579c5e9ce9d6b7e268cb90
Bhaskar Joshi <bhaskarjoshi2545@gmail.com>
2026-03-24 19:51:20 +0530

    Updated dbsetupfile

--- dbsetup.sql
+++ dbsetup.sql
@@ -14,6 +14,14 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '6f4d44d4-f920-11f0-8351-83a09a23def3:1-119';
 
 --
 -- Table structure for table `app_user`
@@ -40,7 +48,7 @@ CREATE TABLE `app_user` (
 
 LOCK TABLES `app_user` WRITE;
 /*!40000 ALTER TABLE `app_user` DISABLE KEYS */;
-INSERT INTO `app_user` VALUES (1,'Bhaskar','Admin','superadmin','admin','Active'),(3,'Rohit','User','rohit123@abc.com','rohit','InActive'),(5,'Mohan','Admin','mohan@123','mohan','Active');
+INSERT INTO `app_user` VALUES (1,'Bhaskar','superadmin','superadmin','admin','Active'),(3,'Rohit','admin','rohit123@abc.com','rohit','Active'),(5,'Mohan','admin','mohan@123','mohan','Active');
 /*!40000 ALTER TABLE `app_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,7 +92,7 @@ CREATE TABLE `customers` (
   `address` text,
   PRIMARY KEY (`customer_id`),
   UNIQUE KEY `mobile` (`mobile`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +101,7 @@ CREATE TABLE `customers` (
 
 LOCK TABLES `customers` WRITE;
 /*!40000 ALTER TABLE `customers` DISABLE KEYS */;
-INSERT INTO `customers` VALUES (1,'Bhaskar','12345','iqwuegdqwbd','qbdiwqdiqwu');
+INSERT INTO `customers` VALUES (1,'Bhaskar','12345','iqwuegdqwbd','qbdiwqdiqwu'),(2,'Nitin','123','nitin@123','abcabc');
 /*!40000 ALTER TABLE `customers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -116,7 +124,7 @@ CREATE TABLE `order_details` (
   KEY `product_id` (`product_id`),
   CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
   CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,7 +133,7 @@ CREATE TABLE `order_details` (
 
 LOCK TABLES `order_details` WRITE;
 /*!40000 ALTER TABLE `order_details` DISABLE KEYS */;
-INSERT INTO `order_details` VALUES (1,1,2,10,90,900),(2,2,1,3,120000,360000),(3,3,2,80,90,7200),(4,4,2,10,90,900),(5,5,2,30,90,2700),(6,6,2,50,90,4500),(7,7,2,4,90,360),(8,8,2,1,90,90),(9,9,2,2,90,180),(10,10,1,4,120000,480000),(11,10,2,1,90,90);
+INSERT INTO `order_details` VALUES (1,1,2,10,90,900),(2,2,1,3,120000,360000),(3,3,2,80,90,7200),(4,4,2,10,90,900),(5,5,2,30,90,2700),(6,6,2,50,90,4500),(7,7,2,4,90,360),(8,8,2,1,90,90),(9,9,2,2,90,180),(10,10,1,4,120000,480000),(11,10,2,1,90,90),(12,11,2,5,90,450),(13,12,2,3,90,270),(14,13,5,1,1000,1000),(15,13,3,5,90,450),(16,13,4,2,30,60),(17,14,2,10,90,900),(18,14,1,1,120000,120000),(19,14,3,5,90,450);
 /*!40000 ALTER TABLE `order_details` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -144,7 +152,7 @@ CREATE TABLE `orders` (
   PRIMARY KEY (`order_id`),
   KEY `customer_id` (`customer_id`),
   CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -153,7 +161,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (1,1,'2026-03-01',900),(2,1,'2026-03-01',360000),(3,1,'2026-03-01',7200),(4,1,'2026-03-01',900),(5,1,'2026-03-01',2700),(6,1,'2026-03-01',4500),(7,1,'2026-03-01',360),(8,1,'2026-03-01',90),(9,1,'2026-03-01',180),(10,1,'2026-03-01',480090);
+INSERT INTO `orders` VALUES (1,1,'2026-03-01',900),(2,1,'2026-03-01',360000),(3,1,'2026-03-01',7200),(4,1,'2026-03-01',900),(5,1,'2026-03-01',2700),(6,1,'2026-03-01',4500),(7,1,'2026-03-01',360),(8,1,'2026-03-01',90),(9,1,'2026-03-01',180),(10,1,'2026-03-01',480090),(11,1,'2026-03-11',450),(12,1,'2026-03-11',270),(13,1,'2026-03-22',1510),(14,1,'2026-03-24',121350);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -172,10 +180,12 @@ CREATE TABLE `products` (
   `min_threshold` int DEFAULT '5',
   `expiry_date` date DEFAULT NULL,
   `category_id` int DEFAULT NULL,
+  `barcode` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`product_id`),
+  UNIQUE KEY `barcode` (`barcode`),
   KEY `category_id` (`category_id`),
   CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -184,7 +194,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'MacBook M3',120000,16,5,'2030-01-01',1),(2,'Cocacola',90,2,5,'2026-03-06',4);
+INSERT INTO `products` VALUES (1,'MacBook M3',120000,15,5,'2030-01-01',1,'194253711444'),(2,'Cocacola',90,40,5,'2026-04-01',4,'5449000000996'),(3,'Sprite',90,40,10,'2026-04-01',4,'5449000011282'),(4,'Paracetamol',30,18,5,'2026-10-02',3,'8901082054230'),(5,'Polo T-Shirts(Men)',1000,19,5,'2027-03-22',6,'8907433285114'),(6,'Baggy Jeans(Men)',1200,100,10,'2027-03-23',6,'8907311102434');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -207,7 +217,7 @@ CREATE TABLE `transactions` (
   KEY `product_id` (`product_id`),
   CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `app_user` (`app_user_pk`),
   CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -216,9 +226,10 @@ CREATE TABLE `transactions` (
 
 LOCK TABLES `transactions` WRITE;
 /*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
-INSERT INTO `transactions` VALUES (1,1,1,'IN',5,'2026-02-26 04:27:14');
+INSERT INTO `transactions` VALUES (1,1,1,'IN',5,'2026-02-26 04:27:14'),(2,1,2,'OUT',10,'2026-02-28 18:30:00'),(3,1,1,'OUT',3,'2026-02-28 18:30:00'),(4,1,2,'OUT',80,'2026-02-28 18:30:00'),(5,1,2,'OUT',10,'2026-02-28 18:30:00'),(6,1,2,'OUT',30,'2026-02-28 18:30:00'),(7,1,2,'OUT',50,'2026-02-28 18:30:00'),(8,1,2,'OUT',4,'2026-02-28 18:30:00'),(9,1,2,'OUT',1,'2026-02-28 18:30:00'),(10,1,2,'OUT',2,'2026-02-28 18:30:00'),(11,1,1,'OUT',4,'2026-02-28 18:30:00'),(12,1,2,'OUT',1,'2026-02-28 18:30:00'),(13,1,2,'OUT',5,'2026-03-10 18:30:00'),(14,1,2,'OUT',3,'2026-03-10 18:30:00'),(15,1,5,'OUT',1,'2026-03-21 18:30:00'),(16,1,3,'OUT',5,'2026-03-21 18:30:00'),(17,1,4,'OUT',2,'2026-03-21 18:30:00'),(18,1,2,'OUT',10,'2026-03-23 18:30:00'),(19,1,1,'OUT',1,'2026-03-23 18:30:00'),(20,1,3,'OUT',5,'2026-03-23 18:30:00'),(33,1,1,'IN',15,'2026-01-01 03:30:00'),(34,1,2,'IN',40,'2026-01-01 03:30:00'),(35,1,3,'IN',40,'2026-01-01 03:30:00'),(36,1,4,'IN',18,'2026-01-01 03:30:00'),(37,1,5,'IN',19,'2026-01-01 03:30:00'),(38,1,6,'IN',100,'2026-01-01 03:30:00');
 /*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
 UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -229,4 +240,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-03-01 16:04:31
+-- Dump completed on 2026-03-24 19:49:50
